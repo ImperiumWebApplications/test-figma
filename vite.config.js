bdff0121ced24543d5ed850863468b69a8e43959
Aditya Singh <adityasingh220598@gmail.com>
2024-04-01 18:02:53 +0530

    Fix: updating configuration

--- vite.config.js
+++ vite.config.js
@@ -10,6 +10,7 @@ export default defineConfig({
       input: {
         main: path.resolve(__dirname, "index.html"),
       },
+      external: ["react", "react-dom"],
     },
     outDir: "dist",
   },
